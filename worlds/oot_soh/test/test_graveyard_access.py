362edbb4fcc7613b4b8ee2e5acf10924b4a42846
lonegraywolf2000 <jafelds@gmail.com>
2025-11-05 08:08:26 -0600

    Have it working. Simplify the test.

--- worlds/oot_soh/test/test_graveyard_access.py
+++ worlds/oot_soh/test/test_graveyard_access.py
@@ -13,14 +13,11 @@ class TestCanReachGraveyardShieldChest(SohTestBase):
         """
         Checking if player can open the shield grave chest by just waiting until the sun sets
         """
-        self.world.glitches_item_name = Items.GLITCHED
-        # self.multiworld.push_precollected(Items.GLITCHED)
         self.world.push_precollected(self.world.create_item(Items.GLITCHED))
         self.multiworld.itempool.append(Item(Items.GLITCHED, ItemClassification.filler, None, 1))
         items = self.collect_by_name(Items.GLITCHED)
         self.assertTrue(len(items) == 1)
-
-        self.multiworld.state.update_reachable_regions(1)        
+      
         self.assertTrue(self.get_item_by_name(Items.GLITCHED).name == 'Glitched Item', f"Glitched Item was found.")
         self.assertTrue(self.can_reach_region(Regions.THE_GRAVEYARD), f"The Graveyard should be accessible.")
         self.assertTrue(self.can_reach_location(Locations.GRAVEYARD_SHIELD_GRAVE_CHEST),
