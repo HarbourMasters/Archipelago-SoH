b9831cc1ad00844721cda821a9d79338112844ac
Scipio Wright <scipiowright@gmail.com>
2025-11-19 22:02:11 -0500

    Fix UT issues with shops, move shop stuff to create_items instead of pre_fill

--- worlds/oot_soh/UniversalTracker.py
+++ worlds/oot_soh/UniversalTracker.py
@@ -84,10 +84,10 @@ def setup_options_from_slot_data(world: "SohWorld") -> None:
             world.options.skeleton_key.value = world.passthrough["skeleton_key"]
             world.options.slingbow_break_beehives.value = world.passthrough["slingbow_break_beehives"]
             world.options.starting_age.value = world.passthrough["starting_age"]
-            world.options.true_no_logic = world.passthrough["no_logic"]
-            world.options.shuffle_tycoon_wallet = world.passthrough["shuffle_tycoon_wallet"]
-            world.options.enable_all_tricks.value = world.passthrough["enable_all_tricks"]
-            world.options.tricks_in_logic.value = world.passthrough["tricks_in_logic"]
+            world.options.true_no_logic.value = world.passthrough["no_logic"]
+            world.options.shuffle_tycoon_wallet.value = world.passthrough.get("shuffle_tycoon_wallet", 1)
+            world.options.enable_all_tricks.value = world.passthrough.get("enable_all_tricks", 0)
+            world.options.tricks_in_logic.value = world.passthrough.get("tricks_in_logic", set())
             # the below do not need to be handled in UT at all, since they do not affect logic
             # shuffle_100_gs_reward, ice_trap_count, ice_trap_filler_replacement, and apworld_version
         else:
